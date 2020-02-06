c06026a59320e6875b3f5723353afe9f405312fd
N3CR0420 <60702307+N3CR0420@users.noreply.github.com>
2020-02-05 19:40:19 -0500

    My first program

---
+++ my-first-program.py
@@ -0,0 +1,15 @@
+### My First Program ###
+
+### Greet the user ###
+print('Hello, new user.')
+
+### Prompt the new user to set his user name ###
+name = input("Please enter your name: ")
+
+print("Welcome,", name)
+
+
+
+
+
+
